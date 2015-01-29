d5c0247e29784f96807594ec01ea7256235edaad
Jonathan <jrhaccount@yahoo.com>
2015-01-29 00:44:11 -0600

    fixed

--- js/app.js
+++ js/app.js
@@ -10,11 +10,43 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
+        {url: "./bower_components/lodash/dist/lodash.min.js"},
         // {url: "./bower_components/backbone/backbone.js"}
+        // {url: "./js/game.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
     })
 
 }
+
+;(function(){
+    var Game = function(canvasId) {
+        var canvas = document.getElementById(canvasId);
+        var screen =Cavnas.getContext('2d');
+        var gameSize = {x: canvas.width, y: canvas.height };
+        console.log("hi")
+
+        var self = this;
+        var tick = function() {
+            self.update();
+            self.draw(screen, gameSize);
+        };
+
+        tick();
+    };
+
+    Game.prototype =  {
+        update: function()  {
+            console.log("hi")
+        },
+
+        draw: function(screen, gameSize) {
+            scree.fillRect(30, 30, 40, 40);
+        }
+    };
+
+    window.onload = function() {
+         new Game("screen");
+    };
+ })();
